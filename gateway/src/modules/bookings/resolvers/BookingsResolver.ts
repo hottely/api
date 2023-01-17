5e1bf8ea59798d830480dd1fe6141bfe00922a1f
Robert Rapiteanu <robert.rapiteanu@gmail.com>
2023-01-17 04:08:16 +0200

    bookings mock

--- gateway/src/modules/bookings/resolvers/BookingsResolver.ts
+++ gateway/src/modules/bookings/resolvers/BookingsResolver.ts
@@ -1,7 +1,15 @@
 import GraphQLJSON from "graphql-type-json";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { MyContext } from "../../../types/MyContext";
 import { RESTDataSource } from "apollo-datasource-rest";
+import {
+  propertiesMock,
+  Property,
+} from "../../property/resolvers/PropertyResolver";
+import CurrentUser from "../../../utils/decorators/currentUser";
+import { User } from "../../../entity/User";
+import { v4 } from "uuid";
+import moment from "moment";
 
 export class BookingAPI extends RESTDataSource {
   override baseURL = `http://bookings-api-service:5000/`;
@@ -9,11 +17,33 @@ export class BookingAPI extends RESTDataSource {
   async getBookings(): Promise<Booking[]> {
     return this.get<Booking[]>("/bookings");
   }
+
+  async getBookingsForUser(userId): Promise<Booking[]> {
+    return this.get<Booking[]>(`/bookings/user/${userId}`);
+  }
+
+  async getBookingsForProperty(propertyId): Promise<Booking[]> {
+    return this.get<Booking[]>(`/bookings${propertyId}`);
+  }
+
+  async createBooking(
+    userId: number,
+    propertyId: string,
+    startDate: string,
+    endDate: string
+  ): Promise<unknown> {
+    return this.post<unknown>(
+      `/bookings${propertyId}/${startDate}/${endDate}/${userId}`
+    );
+  }
 }
 
 type Booking = {
   id: string;
-  name: string;
+  userId: number;
+  property: Property;
+  startDate: string;
+  endDate: string;
 };
 
 @Resolver()
@@ -21,10 +51,81 @@ export class BookingsResolver {
   @Query(() => GraphQLJSON)
   async getBookings(@Ctx() { dataSources }: MyContext): Promise<Booking[]> {
     try {
-      console.log("baseurl", dataSources.bookingsAPI.baseURL);
       return dataSources.bookingsAPI.getBookings();
     } catch (error) {
       throw Error("failed to get bookings");
+    }
+  }
+
+  @Authorized()
+  @Query(() => GraphQLJSON)
+  async getMyBookings(
+    @Ctx() { dataSources }: MyContext,
+    @CurrentUser() currentUser: User
+  ): Promise<Booking[]> {
+    try {
+      const bookingMock = [
+        {
+          id: v4(),
+          userId: currentUser.id,
+          property: propertiesMock[0],
+          startDate: moment().add(8, "weeks").toISOString(),
+          endDate: moment().add(9, "weeks").toISOString(),
+        },
+      ];
+
+      return bookingMock;
+      // return dataSources.bookingsAPI.getBookingsForUser(currentUser.id);
+    } catch (error) {
+      throw Error("failed to get bookings");
+    }
+  }
+
+  @Authorized()
+  @Query(() => GraphQLJSON)
+  async getBookingsForProperty(
+    @Ctx() { dataSources }: MyContext,
+    @CurrentUser() currentUser: User,
+    @Arg("propertyId", () => String) propertyId: string
+  ): Promise<Booking[]> {
+    try {
+      const bookingMock = [
+        {
+          id: v4(),
+          userId: currentUser.id,
+          property: propertiesMock[0],
+          startDate: moment().add(8, "weeks").toISOString(),
+          endDate: moment().add(9, "weeks").toISOString(),
+        },
+      ];
+
+      return bookingMock;
+      // return dataSources.bookingsAPI.getBookingsForProperty(propertyId);
+    } catch (error) {
+      throw Error("failed to get bookings");
+    }
+  }
+
+  @Authorized()
+  @Mutation(() => Boolean)
+  async createBooking(
+    @Ctx() { dataSources }: MyContext,
+    @CurrentUser() currentUser: User,
+    @Arg("propertyId", () => String) propertyId: string,
+    @Arg("startDate", () => String) startDate: string,
+    @Arg("endDate", () => String) endDate: string
+  ) {
+    try {
+      // await dataSources.bookingsAPI.createBooking(
+      //   currentUser.id,
+      //   propertyId,
+      //   startDate,
+      //   endDate
+      // );
+
+      return true;
+    } catch (error) {
+      throw Error("failed to create booking");
     }
   }
 }
--- gateway/src/modules/property/resolvers/PropertyResolver.ts
+++ gateway/src/modules/property/resolvers/PropertyResolver.ts
@@ -1,7 +1,7 @@
 import GraphQLJSON from "graphql-type-json";
 import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { MyContext } from "../../../types/MyContext";
-import { RESTDataSource } from "apollo-datasource-rest";
+import { RequestOptions, RESTDataSource } from "apollo-datasource-rest";
 import CurrentUser from "../../../utils/decorators/currentUser";
 import { User } from "../../../entity/User";
 import { PropertyUploadInput } from "../inputs/PropertyUploadInput";
@@ -9,12 +9,33 @@ import { PropertyUploadInput } from "../inputs/PropertyUploadInput";
 export class PropertyAPI extends RESTDataSource {
   override baseURL = `http://property-service:5000/`;
 
+  protected willSendRequest?(request: RequestOptions) {
+    if (this.context.req.headers?.authorization) {
+      request.headers.set(
+        "Authorization",
+        this.context.req.headers.authorization
+      );
+    }
+  }
+
   async getProperties(): Promise<Property[]> {
     return this.get<Property[]>("properties");
   }
+
+  async getProperty(propertyId: string): Promise<Property> {
+    return this.get<Property>(`properties/${propertyId}`);
+  }
+
+  async getListings(): Promise<Property[]> {
+    return this.get<Property[]>(`listings`);
+  }
+
+  async createProperty(data: PropertyUploadInput): Promise<unknown> {
+    return this.post<unknown>(`properties`, data);
+  }
 }
 
-type Property = {
+export type Property = {
   id: string;
   name: string;
   description: string;
@@ -24,13 +45,16 @@ type Property = {
   price: number;
   images: { url: string }[];
   amenities: { name: string }[];
-  landlord_id: string;
+  landlord_id: number;
 };
 
 const thumbnailUrl =
   "https://a0.muscache.com/im/pictures/a139a0fd-efbe-4b16-a5bb-18ec5671fe56.jpg?im_w=1200";
 
-const propertiesMock: Property[] = [
+const secondImage =
+  "https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+
+export const propertiesMock: Property[] = [
   {
     id: "1",
     name: "Beautiful apartment in the heart of the city",
@@ -39,9 +63,9 @@ const propertiesMock: Property[] = [
     bathrooms: 1,
     bedrooms: 2,
     price: 100,
-    images: [{ url: thumbnailUrl }],
+    images: [{ url: thumbnailUrl }, { url: secondImage }],
     amenities: [{ name: "parking" }],
-    landlord_id: "1",
+    landlord_id: 1,
   },
 ];
 
@@ -63,6 +87,7 @@ export class PropertyResolver {
     @Arg("propertyId", () => String) propertyId: string
   ) {
     return propertiesMock[0];
+    // return dataSources.propertyAPI.getProperty(propertyId);
   }
 
   @Authorized()
@@ -72,8 +97,13 @@ export class PropertyResolver {
     @CurrentUser() currentUser: User,
     @Arg("data") data: PropertyUploadInput
   ) {
-    const userId = currentUser.id;
-    return true;
+    try {
+      await dataSources.propertyAPI.createProperty(data);
+      return true;
+    } catch (error) {
+      console.log("error at createProperty", error);
+      throw Error("failed to create property");
+    }
   }
 
   @Authorized()
@@ -83,10 +113,8 @@ export class PropertyResolver {
     @CurrentUser() currentUser: User
   ): Promise<Property[]> {
     try {
-      const userId = currentUser.id;
-
-      // return dataSources.propertyAPI.getListings(userId);
       return propertiesMock;
+      // return dataSources.propertyAPI.getListings();
     } catch (error) {
       throw Error("failed to get listings");
     }
